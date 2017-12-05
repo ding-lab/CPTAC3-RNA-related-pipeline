5f7b6140c4a29342eed0e04a0949157a07f718f7
Qingsong Gao <qgao@linus300.gsc.wustl.edu>
2017-12-05 15:03:45 -0600

    Updated get_link.pl file

--- get_link.pl
+++ get_link.pl
@@ -3,22 +3,40 @@
 use strict;
 use warnings;
 
+die "Usage: $0  \"file_from_Matt\" " if (@ARGV < 1);
 
 open(OUT, ">to_run.sh");
 print OUT "#!/bin/bash\n";
 
-open(IN, "/gscuser/mwyczalk/projects/CPTAC3/import.CPTAC3b1/BamMap/CPTAC3.b1.RNA-Seq.BamMap.dat");
+open(IN, "$ARGV[0]");
+<IN>;
+my $header = <IN>;
+die "Matt has changed the format of input file!\n" if $header!~m/^#/;
+die "Matt has changed the format of input file!\n" if $header!~m/Case/;
+die "Matt has changed the format of input file!\n" if $header!~m/Disease/;
+die "Matt has changed the format of input file!\n" if $header!~m/DataPath/;
+my @h = split(/\t/, $header);
+my ($sample_idx, $disease_idx, $datapath_idx);
+for(my $i=0; $i<=$#h; $i++)
+{
+        $sample_idx = $i if $h[$i] eq "Case";
+        $disease_idx = $i if $h[$i] eq "Disease";
+        $datapath_idx = $i if $h[$i] eq "DataPath";
+}
+
+open(IN, "sort -k 1,1 $ARGV[0] |");
 while(<IN>)
 {
 	chomp;
 	next if $_=~m/^#/;
 	my @l = split(/\t/,);
-	my $sample=$l[2]."\_\_".substr($l[0], 0, -13);
-	system("mkdir -p $sample");
-	system("ln -s $l[5] $sample/$sample\_1.fastq.gz");
+	my $sample=$l[$disease_idx]."\_\_".$l[$sample_idx];
 	my $nxt = <IN>;
 	my @m = split(/\t/,$nxt);
-        system("ln -s $m[5] $sample/$sample\_2.fastq.gz");
+	die "Not paired!\n" if $l[$sample_idx] ne $m[$sample_idx];
+	system("mkdir -p $sample");
+        system("ln -s $l[$datapath_idx] $sample/$sample\_1.fastq.gz");
+        system("ln -s $m[$datapath_idx] $sample/$sample\_2.fastq.gz");
 	print OUT "lsf_submit 50 4 $sample.log bash /gscmnt/gc2521/dinglab/qgao/Scripts/RNA/rna_pipeline.sh $sample $sample\_1.fastq.gz $sample\_2.fastq.gz\n";
 }
 
